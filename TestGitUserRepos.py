073b7daffa5d023c1b28a9fa96ec9c2cc065e545
Nicole <nicolegonsalves894@gmail.com>
2021-03-09 11:13:47 -0500

    Ading travis.yml file

--- TestGitUserRepos.py
+++ TestGitUserRepos.py
@@ -1,9 +1,24 @@
 import unittest
-
 from GetUserRepos import get_repo_commits
+
 
 class TestGitUserRepos(unittest.TestCase):
 
     def testCorrectRepos(self):
-        result = self.get_repo_commits('nicole84')
+        result = get_repo_commits('nicole894')
         self.assertEqual(len(result), 12)
+
+    def testInValidRepos(self):
+         result = get_repo_commits('nicole894')
+         x =  result[1]
+         self.assertEqual(x['Number of commits'], 3)
+    #
+    # def testCorrectCommits(self):
+    #     result = get_repo_commits('nicole894')
+    #     x = result[1]
+    #     self.assertEqual(x['Number of commits'], 2)
+
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
